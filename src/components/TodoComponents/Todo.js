a9d0bbb840f963af8a2a5ed773b5351a6f953788
timislearning <tim.mitzen@gmail.com>
2019-10-03 10:12:46 -0700

    mvp

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react'
+
+
+export default class Todo extends Component {
+   render() {
+      return (
+         
+      <div className={ this.props.todo.completed ? 'item': ""} onClick={(event)=>this.props.toggleTodo(event, this.props.todo.id)}> 
+      
+
+   
+   
+    
+   <p>{this.props.todo.id}</p>
+   
+   
+   </div>  
+   );
+};
+         
+      
+   }
+
+
+
+  
